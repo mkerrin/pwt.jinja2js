dcf075fd6b3794e2b679fbfb8ab015be5560406c
Michael Kerrin <michael.kerrin@gmail.com>
2011-02-10 18:12:13 +0000

    Render template for application.

--- src/jscomp/app.py
+++ src/jscomp/app.py
@@ -1,6 +1,10 @@
 import webob
 import webob.dec
 
+from jinja import PackageLoader, Environment
+
 @webob.dec.wsgify.middleware
 def main(request, config, **kwargs):
-    return webob.Response("Hello, world")
+    env = Environment(loader = PackageLoader("jscomp", "templates"))
+    template = env.get_template("hello.html")
+    return webob.Response(template.render())
