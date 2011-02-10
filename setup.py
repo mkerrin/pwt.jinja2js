4205dc596e2eb5baf1b7d7a0bc66ac18867ad591
Michael Kerrin <michael.kerrin@gmail.com>
2011-02-10 18:11:56 +0000

    Speed up Jinja2

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
 
     install_requires = ["setuptools",
                         "WebOb",
+                        "MarkupSafe",
                         "Jinja",
                         ],
 
