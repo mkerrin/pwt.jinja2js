047296cddb27f81d4ffe5bc1017cc95711b9781c
Michael Kerrin <michael.kerrin@gmail.com>
2011-02-15 13:17:36 +0000

    Configure the application without a global env object.

--- src/jscomp/app.py
+++ src/jscomp/app.py
@@ -1,12 +1,24 @@
 import webob
 import webob.dec
 
-from jinja2 import PackageLoader, Environment
+import jinja2
 
-env = Environment(loader = PackageLoader("jscomp", "templates"))
-env.compile_templates("archive", zip = None)
+class main(object):
 
-@webob.dec.wsgify.middleware
-def main(request, config, **kwargs):
-    template = env.get_template("hello.html")
-    return webob.Response(template.render())
+    def __init__(self, *args, **kwargs):
+        loaders = []
+        for package in kwargs["packages"].split():
+            loaders.append(jinja2.PackageLoader(*package.split(":")))
+
+        if len(loaders) == 1:
+            loader = loaders[0]
+        else:
+            loader = jinja2.ChoiceLoader(loaders)
+
+        self.env = jinja2.Environment(
+            loader = loader, extensions = ["jscomp.jscompiler.Namespace"])
+
+    @webob.dec.wsgify
+    def __call__(self, request):
+        template = self.env.get_template("hello.html")
+        return webob.Response(template.render(data = [1, 3, 5]))
