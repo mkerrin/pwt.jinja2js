40470116e50171d5eff02657372159b720aa8c3c
Michael Kerrin <michael.kerrin@gmail.com>
2011-02-21 11:00:21 +0000

    Move the closurebuilder recipe to pwt.recipe.closurebuilder

---
+++ pwt.recipe.closurebuilder/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "pwt.recipe.closurebuilder",
+    author = "Michael Kerrin",
+    author_email = "michael.kerrin@gmail.com",
+
+    packages = find_packages("src"),
+    package_dir = {"": "src"},
+
+    install_requires = ["setuptools",
+                        "zc.buildout",
+                        ],
+
+    entry_points = """
+[zc.buildout]
+dependency = pwt.recipe.closurebuilder:Deps
+""",
+
+    include_package_data = True,
+    zip_safe = False,
+    )
