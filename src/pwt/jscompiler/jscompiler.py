1d720174c55a91f8ce09fb5c932f3b3ca118a845
Michael Kerrin <michael.kerrin@gmail.com>
2011-03-22 13:51:36 +0000

    Remove filters file - and move back into jscompiler.

--- src/pwt/jscompiler/jscompiler.py
+++ src/pwt/jscompiler/jscompiler.py
@@ -49,6 +49,16 @@ OPERATORS = {
     }
 
 
+def generate(node, environment, name, filename, stream = None):
+    """Generate the python source for a node tree."""
+    if not isinstance(node, jinja2.nodes.Template):
+        raise TypeError("Can't compile non template nodes")
+    generator = CodeGenerator(environment, name, filename, stream)
+    generator.visit(node)
+    if stream is None:
+        return generator.stream.getvalue()
+
+
 class JSFrameIdentifierVisitor(jinja2.compiler.FrameIdentifierVisitor):
 
     def __init__(self, identifiers, environment, ctx):
@@ -769,14 +779,49 @@ class register_filter(object):
 
         return func
 
-import filters
+
+@register_filter("default")
+def filter_default(generator, node, frame, default_value = ""):
+    generator.visit(node.node, frame)
+    generator.write(" ? ")
+    generator.visit(node.node, frame)
+    generator.write(" : ")
+    generator.visit(default_value, frame)
 
 
-def generate(node, environment, name, filename, stream = None):
-    """Generate the python source for a node tree."""
-    if not isinstance(node, jinja2.nodes.Template):
-        raise TypeError("Can't compile non template nodes")
-    generator = CodeGenerator(environment, name, filename, stream)
-    generator.visit(node)
-    if stream is None:
-        return generator.stream.getvalue()
+@register_filter("truncate")
+def filter_truncate(generator, node, frame, length):
+    generator.visit(node.node, frame)
+    generator.write(".substring(0, ")
+    generator.visit(length, frame)
+    generator.write(")")
+
+
+@register_filter("capitalize")
+def filter_capitalize(generator, node, frame):
+    generator.visit(node.node, frame)
+    generator.write(".substring(0, 1).toUpperCase(), ")
+    generator.visit(node.node, frame)
+    generator.write(".substring(1)")
+
+
+@register_filter("last")
+def filter_last(generator, node, frame):
+    generator.visit(node.node, frame)
+    generator.write(".pop()")
+
+
+@register_filter("length")
+def filter_length(generator, node, frame):
+    generator.visit(node.node, frame)
+    generator.write(".length")
+
+
+@register_filter("replace")
+def filter_replace(generator, node, frame, old, new): #, count = None)
+    generator.visit(node.node, frame)
+    generator.write(".replace(")
+    generator.visit(old, frame)
+    generator.write(", ")
+    generator.visit(new, frame)
+    generator.write(")")
