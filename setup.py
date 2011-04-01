048ffb35e22f6f5114807709e72392ab34368323
Michael Kerrin <michael.kerrin@gmail.com>
2011-04-01 16:15:38 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "pwt.jinja2js",
-    version = "0.1",
+    version = "0.5",
 
     author = "Michael Kerrin",
     author_email = "michael.kerrin@gmail.com",
