3ad88b2d676fed706895bde68ca2acc3fa1a72fd
Michael Kerrin <michael.kerrin@gmail.com>
2013-02-28 12:02:59 +0000

    Update version of pwt.jinja2js

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "pwt.jinja2js",
-    version = "0.7.4",
+    version = "0.8.0",
 
     author = "Michael Kerrin",
     author_email = "michael.kerrin@gmail.com",
